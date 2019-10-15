8d68384bbba48d8031ebeabcedf921fb37e2f94b
XiaoQu <lqqu@thoughtworks.com>
2019-10-15 10:51:51 +0800

    fellow using

--- test/NetArchTest.Rules.UnitTests/TypesTests.cs
+++ test/NetArchTest.Rules.UnitTests/TypesTests.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace NetArchTest.Rules.UnitTests
+﻿namespace NetArchTest.Rules.UnitTests
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Reflection;
